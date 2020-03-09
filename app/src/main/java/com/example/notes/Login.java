9c01ed4dfcd1f8d7f97b524290ddde123398b93a
Pedro Dias <pedrodias@ipvc.pt>
2020-03-09 18:02:18 +0000

    Login screen frist to appear

--- app/src/main/java/com/example/notes/Login.java
+++ app/src/main/java/com/example/notes/Login.java
@@ -3,21 +3,49 @@ package com.example.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
     EditText email;
     EditText password;
     Button loginbtn;
+    TextView registerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        email = findViewById(R.id.email);
+        email = findViewById(R.id.loginemail);
+        password = findViewById(R.id.loginpassword);
+        registerText = findViewById(R.id.registerText);
+        loginbtn = findViewById(R.id.loginBtn);
+
+
+        // Register text
+        registerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Login.this, "Register text", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Login Button
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Login.this, "Login BTN", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
 
 
 
--- app/src/main/java/com/example/notes/Register.java
+++ app/src/main/java/com/example/notes/Register.java
@@ -3,12 +3,41 @@ package com.example.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
+
+
+    Button registerBtn;
+    TextView name;
+    TextView email;
+    TextView password;
+    TextView confirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        registerBtn = findViewById(R.id.registerBtn);
+        name = findViewById(R.id.registername);
+        email = findViewById(R.id.registeremail);
+        password = findViewById(R.id.registerpassword);
+        confirmPassword = findViewById(R.id.registerconfirmpassword);
+
+
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Register.this, "Register BTN", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
 }
