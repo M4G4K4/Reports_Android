3eb6d5d9fef9d3f457d2dd36696df5f6b3217946
Pedro Dias <pedrods@ipvc.pt>
2020-03-22 19:26:13 +0000

    Get Reports from database

--- app/src/main/java/com/example/notes/Maps.java
+++ app/src/main/java/com/example/notes/Maps.java
@@ -4,11 +4,23 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.android.volley.NetworkError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,22 +29,39 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Random;
+import java.util.Set;
 
 public class Maps extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
     // How to use Glide:
     // Glide.with(context).load("YourUrl").into(imageView);
 
     Integer userID;
+    String imgURL;
+
+
+    ArrayList<HashMap<String, String>> location = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -49,19 +78,17 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(Maps.this, "Fab cliked", Toast.LENGTH_SHORT).show();
                 addReport();
             }
         });
 
 
-        // Get all reports from database
-        requestReports();
     }
 
+
+    // Fab icon cliked , open popup
     private void addReport() {
         Intent intent = new Intent(getBaseContext(), popup.class);
-        System.out.println("Maps userID : " + userID);
         intent.putExtra("UserID", userID);
         startActivity(intent);
 
@@ -83,20 +110,93 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
+/*
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        LatLng lisbon = new LatLng(38.736946,-9.142685 );
+        LatLng braga = new LatLng(41.55032, -8.42005);
 
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.addMarker(new MarkerOptions().position(lisbon).title("Lisbon marker"));
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(lisbon));
+        mMap.addMarker(new MarkerOptions().position(braga).title("Braga marker"));
+*/
+
+
+        /*
+        for (int i= 0; i< location.size() ;i++){
+            System.out.println("adadadadadadadadadadadad");
+            LatLng points = new LatLng( Double.parseDouble(location.get(i).get("latitude")) , Double.parseDouble(location.get(i).get("longitude")) );
+            mMap.addMarker(new MarkerOptions().position(points).title(location.get(i).get("description")));
+        }
+*/
+        System.out.println("Before for Size: " + location.size());
+        for (int a =0; a<location.size();a++) {
+
+            HashMap<String, String> tmpData = (HashMap<String, String>) location.get(a);
+            Set<String> key = tmpData.keySet();
+            Iterator it = key.iterator();
+            while (it.hasNext()) {
+                String hmKey = (String)it.next();
+                String hmData = tmpData.get(hmKey);
+
+                System.out.println("Key: "+hmKey +" & Data: "+hmData);
+                it.remove(); // avoids a ConcurrentModificationException
+            }
+
+        }
 
     }
 
 
-    private void requestReports() {
+    private void requestReports() throws JSONException {
+        String url ="http://64.227.36.62/api/getAllReports";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try{
+                            HashMap<String, String> map = new HashMap<String, String>();
+                            // Loop through the array elements
+                            for(int i=0;i<response.length();i++){
+                                // Get current json object
+                                JSONObject data = response.getJSONObject(i);
+
+                                map.put("description", data.getString("description"));
+                                map.put("longitude",data.getString("longitude"));
+                                map.put("latitude",data.getString("latitude"));
+                                map.put("img",data.getString("img"));
+                                map.put("morada",data.getString("morada"));
+                                map.put("userID",data.getString("userID"));
+                                
+                                location.add(map);
+
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        System.out.println("Response Error");
+                    }
+                }
+        );
+
+
+        queue.add(jsonArrayRequest);
 
     }
+
+
+
+
+
+
 
 }
